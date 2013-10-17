4753ce3882ef4f7eea64bceb87b68fc847f79959
Harshawardhan Gadgil <hgadgil@rbcon.com>
2013-10-17 13:50:08 -0700

    Add postgresql and dev libs to base packages

--- setup/install_base_packages.sh
+++ setup/install_base_packages.sh
@@ -4,4 +4,4 @@ set -e
 
 apt-get update -y --fix-missing
 apt-get install -y build-essential curl git-core libcurl4-gnutls-dev libmysqlclient-dev libaio1 nodejs
-
+apt-get install -y libpq-dev postgresql
